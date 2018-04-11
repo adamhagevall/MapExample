c588037ccea2610d43cc56722e90fd43f455a77e
cawik <c.wikstrom@outlook.com>
2018-04-11 16:43:38 +0200

    ändrade lite i LibraryList för att få det att funka på android

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -60,7 +60,7 @@ class LibraryList extends Component {
         case true:
             return (
             <View>
-            <Button onPress={this.onButtonPress}>First</Button>;
+            <Button onPress={this.onButtonPress}>First</Button>
             <View>
             {this.renderList()}
             </View>
@@ -69,7 +69,7 @@ class LibraryList extends Component {
         case false: 
         return (
             <View>
-            <Button onPress={this.onButtonPress}>Second</Button>;
+            <Button onPress={this.onButtonPress}>Second</Button>
             <View>
             {this.renderMap()}
             </View>
@@ -77,7 +77,11 @@ class LibraryList extends Component {
             );
 
         default: 
-            return <Button onPress={this.onButtonPress}>Third</Button>;
+            return (
+            <View>
+            <Button onPress={this.onButtonPress}>Third</Button>
+            </View>
+            );
     }
 }
 
