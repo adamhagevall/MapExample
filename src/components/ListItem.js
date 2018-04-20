af05f48bd4bf86597aa208c2ac3d6685248459fe
Ellen Kyhle <ellenkyhle@student243-173.eduroam.uu.se>
2018-04-20 12:34:44 +0200

    ändringar i pilar på ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -17,6 +17,12 @@ class ListItem extends Component {
         LayoutAnimation.easeInEaseOut();
     }
 
+    //tillagt
+    constructor() {
+        super();
+        this.state = { informationExpanded: false };
+      }
+
     renderDescription() {
         const { library, expanded } = this.props;
 
@@ -30,20 +36,62 @@ class ListItem extends Component {
             );
         }
     }
+
+    //tillagt
+    renderIcon() {
+        console.log("in renderIcon")
+        if (this.state.informationExpanded) {
+            this.setState({ informationExpanded: false })
+        }
+        else {
+            this.setState({ informationExpanded: true })
+        }
+    }
+
+    renderInformation(selectedID) {
+        console.log("in renderInformation")
+        console.log("Current state of informationExpanded: "+this.state.informationExpanded)
+        console.log("current id: "+selectedID)
+
+        if(this.state.informationExpanded) {
+            this.props.selectLibrary(100)
+            console.log("this information is already expanded!")
+        }
+        else {
+            this.props.selectLibrary(selectedID)
+            console.log("Vanliga funtionen selectLibrary")
+        }
+    }
+
+    buttonFunction(selectedID) {
+        this.renderIcon();
+        this.renderInformation(selectedID)
+    }
+
     render() {
         const { titleStyle } = styles;
         const { id, title } = this.props.library;
 
+        //tillagt
+        const expandIcon = "arrow-down"
+        const shrinkIcon = "arrow-up"
+
         return (
             <TouchableWithoutFeedback
-                onPress={() => this.props.selectLibrary(id) && console.log('fkn2')}
+             //   onPress={() => this.props.selectLibrary(id) && this.setState({ informationExpanded: true })}
+            // onPress={() => this.props.selectLibrary(id) && this.renderIcon()}
+            // onPress={() => this.renderIcon() && this.renderInformation(id)}
+            onPress={() => this.buttonFunction(id)}
+
+
             >
                 <View>
                     <CardSection>
                         <Text style={titleStyle}>
                         {title}
                         </Text>
-                        <Icon name="arrow-down" style={{ color: 'black', left: 200 }} />
+                        {/* <Icon name= "arrow-down" style={{ color: 'black', left: 200 }} /> */}
+                        <Icon name={(this.state.informationExpanded === true) ? shrinkIcon : expandIcon} style={{ color: 'black', left: 200 }} />
                     </CardSection>
                     {this.renderDescription()}
                 </View>
