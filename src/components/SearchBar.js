2ee6a22b20287997b2f6c74f02725e1d4bcb0a57
Adam Hagevall <adamhagevall@Adams-MBP.lan>
2018-06-06 17:11:36 +0200

    Småfix i searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -70,32 +70,28 @@ export default class SearchBar extends Component {
     }
 
     render() {
-
         let label = this.props.placeholder;
-
+        console.log(this.props.booleanFromParent)
         return (
             <GooglePlacesAutocomplete
                 placeholder={label}
                 minLength={2} // minimum length of text to search
                 autoFocus={false}
                 returnKeyType={'default'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
-                listViewDisplayed={false}    // true/false/undefined
+                listViewDisplayed={this.props.booleanFromParent}    // true/false/undefined
                 fetchDetails={true}
                 renderDescription={row => row.description} // custom description render
                 onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
                     { this.componentWillMount(), this.sendDetails(details.geometry.location) };
                 }
                 }
-
                 getDefaultValue={() => ''}
-
                 query={{
                     // available options: https://developers.google.com/places/web-service/autocomplete
                     key: 'AIzaSyAVoFdCevwg5QcC11Kzn3AOKS_UcGFgvMk',
                     language: 'sv', // language of the results
                     types: 'address' // default: 'geocode'
                 }}
-
                 styles={{
                     textInputContainer: {
                         flex: 1,
@@ -125,10 +121,11 @@ export default class SearchBar extends Component {
                     },
                     description: {
                         fontWeight: 'bold',
-                        position: 'relative'
+                        position: 'relative',
                     },
                     predefinedPlacesDescription: {
-                        color: '#1faadb'
+                        // color: '#1faadb'
+                        color: 'black'
                         //position: 'absolute'
                         //backgroundColor: 'blue'
                     }
@@ -145,10 +142,8 @@ export default class SearchBar extends Component {
                     rankby: 'distance',
                     types: 'food'
                 }}
-
                 filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
                 predefinedPlaces={[this.state.currentPosition, availabilityArena, Donnersplats, Storatorget, stHansplan, Almedalsscenen, scandicHotel, toilets, parkingLots]}
-
                 debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
             //   renderLeftButton={()  => <Image source={require('path/custom/left-icon')} />}
             //   renderRightButton={() => <Text>Custom text after the input</Text>}
